554285dcd8610618f579e0bd5a5b129def4df119
Sorin S <sts221@gmail.com>
2024-07-06 00:15:09 -0400

    Before submit V1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/Menu";
 import HomeComponent from "./components/HomeComponent";
